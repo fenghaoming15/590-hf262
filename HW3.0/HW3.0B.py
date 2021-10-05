4527c8d9b96b0b28c44ab7172b8a9a99ab5113a7
Haoming Feng <haomingfeng@macbook-pro.myfiosgateway.com>
2021-10-05 10:54:56 -0400

    hw3

--- HW3.0/HW3.0B.py
+++ HW3.0/HW3.0B.py
@@ -12,26 +12,26 @@ import json
 #------------------------
 
 #MISC PARAMETERS
-IPLOT		=	True
-I_NORMALIZE =	True;
-# model_type 	=	"linear";  
-# model_type 	=	"logistic";  	 
-model_type 	=	"ANN";  		
+IPLOT        =    True
+I_NORMALIZE =    True;
+# model_type     =    "linear";  
+# model_type     =    "logistic";       
+model_type     =    "ANN";          
 
 #OPTIMIZATION PARAM
-PARADIGM 	=	'batch'
-algo 		=	"MOM"	#GD OR MOM
-LR 			=	0.1  	#LEARNING RATE
-dx 			=	0.0001	#STEP SIZE FOR FINITE DIFFERENCE
-max_iter 	=	2000	#MAX NUMBER OF ITERATION
-tol 		= 	10**-10	#EXIT AFTER CHANGE IN LOSS IS LESS THAN THIS 
-max_rand_wb	=	1.0 	#MAX FOR RANDOM INITIAL GUESS
-GAMMA_L1	=	0.0		#L1 REGULARIZATION CONSTANT
-GAMMA_L2	=	0.01	#L2 REGULARIZATION CONSTANT
-alpha		=	0.25	#MOMENTUM PARAMETER
+PARADIGM     =    'batch'
+algo         =    "MOM"    #GD OR MOM
+LR             =    0.1      #LEARNING RATE
+dx             =    0.0001    #STEP SIZE FOR FINITE DIFFERENCE
+max_iter     =    2000    #MAX NUMBER OF ITERATION
+tol         =     10**-10    #EXIT AFTER CHANGE IN LOSS IS LESS THAN THIS 
+max_rand_wb    =    1.0     #MAX FOR RANDOM INITIAL GUESS
+GAMMA_L1    =    0.0        #L1 REGULARIZATION CONSTANT
+GAMMA_L2    =    0.01    #L2 REGULARIZATION CONSTANT
+alpha        =    0.25    #MOMENTUM PARAMETER
 #ANN PARAM
-layers		=	['overwrite',5,5,1] #FIRST NUMBER OVERWRITTEN LATER BY INPUT SHAPE
-activation	=	"TANH"	#SIGMOID OR TAHN
+layers        =    ['overwrite',5,5,1] #FIRST NUMBER OVERWRITTEN LATER BY INPUT SHAPE
+activation    =    "TANH"    #SIGMOID OR TAHN
 
 #SAVE HISTORY FOR PLOTTING AT THE END
 epoch=1; epochs=[]; loss_train=[];  loss_val=[]
@@ -86,7 +86,7 @@ for i in range(0,len(x)):
 
 #MAKE ROWS=SAMPLE DIMENSION (TRANSPOSE)
 X=np.array(xtmp); Y=np.array(ytmp)
-NFIT=X.shape[1]+1  		#plus one for the bias term
+NFIT=X.shape[1]+1          #plus one for the bias term
 
 #SIGMOID
 def S(x): return 1.0/(1.0+np.exp(-x))
@@ -99,22 +99,22 @@ YMEAN=np.mean(Y,axis=0); YSTD=np.std(Y,axis=0)
 
 #TAKE MEAN AND STD DOWN COLUMNS (I.E DOWN SAMPLE DIMENSION)
 if(I_NORMALIZE):
-	X=(X-XMEAN)/XSTD;  Y=(Y-YMEAN)/YSTD  
-	I_UNNORMALIZE=True
+    X=(X-XMEAN)/XSTD;  Y=(Y-YMEAN)/YSTD  
+    I_UNNORMALIZE=True
 else:
-	I_UNNORMALIZE=False
+    I_UNNORMALIZE=False
 
 #------------------------
 #PARTITION DATA
 #------------------------
-#TRAINING: 	 DATA THE OPTIMIZER "SEES"
+#TRAINING:      DATA THE OPTIMIZER "SEES"
 #VALIDATION: NOT TRAINED ON BUT MONITORED DURING TRAINING
-#TEST:		 NOT MONITORED DURING TRAINING (ONLY USED AT VERY END)
+#TEST:         NOT MONITORED DURING TRAINING (ONLY USED AT VERY END)
 
 f_train=0.8; f_val=0.15; f_test=0.05;
 
 if(f_train+f_val+f_test != 1.0):
-	raise ValueError("f_train+f_val+f_test MUST EQUAL 1");
+    raise ValueError("f_train+f_val+f_test MUST EQUAL 1");
 
 #PARTITION DATA
 rand_indices = np.random.permutation(X.shape[0])
@@ -128,160 +128,177 @@ print("test_idx shape:" ,test_idx.shape)
 
 
 def extract_submatrices(WB):
-	submatrices=[]; K=0
-	for i in range(0,len(layers)-1):
-		#FORM RELEVANT SUB MATRIX FOR LAYER-N
-		Nrow=layers[i+1]; Ncol=layers[i] #+1
-		w=np.array(WB[K:K+Nrow*Ncol].reshape(Ncol,Nrow).T) #unpack/ W 
-		K=K+Nrow*Ncol; #print i,k0,K
-		Nrow=layers[i+1]; Ncol=1; #+1
-		b=np.transpose(np.array([WB[K:K+Nrow*Ncol]])) #unpack/ W 
-		K=K+Nrow*Ncol; #print i,k0,K
-		submatrices.append(w); submatrices.append(b)
-		#print(w.shape,b.shape)
-	return submatrices
+    submatrices=[]; K=0
+    for i in range(0,len(layers)-1):
+        #FORM RELEVANT SUB MATRIX FOR LAYER-N
+        Nrow=layers[i+1]; Ncol=layers[i] #+1
+        w=np.array(WB[K:K+Nrow*Ncol].reshape(Ncol,Nrow).T) #unpack/ W 
+        K=K+Nrow*Ncol; #print i,k0,K
+        Nrow=layers[i+1]; Ncol=1; #+1
+        b=np.transpose(np.array([WB[K:K+Nrow*Ncol]])) #unpack/ W 
+        K=K+Nrow*Ncol; #print i,k0,K
+        submatrices.append(w); submatrices.append(b)
+        #print(w.shape,b.shape)
+    return submatrices
 
 if(model_type=="ANN"):
-	layers[0]		=	 X.shape[1]  	#OVERWRITE WITH INPUT SIZE
+    layers[0]        =     X.shape[1]      #OVERWRITE WITH INPUT SIZE
 
-	#CALCULATE NUMBER OF FITTING PARAMETERS FOR SPECIFIED NN 
-	NFIT=0; 
-	for i in range(1,len(layers)):  
-		NFIT=NFIT+layers[i-1]*layers[i]+layers[i]
-	print("NFIT		:	",NFIT)
+    #CALCULATE NUMBER OF FITTING PARAMETERS FOR SPECIFIED NN 
+    NFIT=0; 
+    for i in range(1,len(layers)):  
+        NFIT=NFIT+layers[i-1]*layers[i]+layers[i]
+    print("NFIT        :    ",NFIT)
     
 #------------------------
 #MODEL
 #------------------------
 def model(x,p):
-	if(model_type=="linear" or model_type=="logistic" ):   
-		out=p[0]+np.matmul(x,p[1:].reshape(NFIT-1,1))
-		if(model_type=="logistic"): out=S(out)
-		return  out  
+    if(model_type=="linear" or model_type=="logistic" ):   
+        out=p[0]+np.matmul(x,p[1:].reshape(NFIT-1,1))
+        if(model_type=="logistic"): out=S(out)
+        return  out  
 
-	if(model_type=="ANN"):
-		submatrices=extract_submatrices(p)
-		return  NN_eval(x,submatrices)
+    if(model_type=="ANN"):
+        submatrices=extract_submatrices(p)
+        return  NN_eval(x,submatrices)
+
 
 #FUNCTION TO MAKE VARIOUS PREDICTIONS FOR GIVEN PARAMETERIZATION
 def predict(p):
-	global YPRED_T,YPRED_V,YPRED_TEST,MSE_T,MSE_V
-	YPRED_T=model(X[train_idx],p)
-	YPRED_V=model(X[val_idx],p)
-	YPRED_TEST=model(X[test_idx],p)
-	MSE_T=np.mean((YPRED_T-Y[train_idx])**2.0)
-	MSE_V=np.mean((YPRED_V-Y[val_idx])**2.0)
+    global YPRED_T,YPRED_V,YPRED_TEST,MSE_T,MSE_V
+    YPRED_T=model(X[train_idx],p)
+    YPRED_V=model(X[val_idx],p)
+    YPRED_TEST=model(X[test_idx],p)
+    MSE_T=np.mean((YPRED_T-Y[train_idx])**2.0)
+    MSE_V=np.mean((YPRED_V-Y[val_idx])**2.0)
 
 #------------------------
 #LOSS FUNCTION
 #------------------------
 
 def loss(p,index_2_use):
-	errors=model(X[index_2_use],p)-Y[index_2_use]  #VECTOR OF ERRORS
-	out=np.mean(errors**2.0)				#MSE
-	if(GAMMA_L1!=0.0):
-		out=out+GAMMA_L1*np.sum(np.absolute(p))
-	if(GAMMA_L2!=0.0):
-		out=out+GAMMA_L2*np.sum(p**2.0)
-	return out
+    errors=model(X[index_2_use],p)-Y[index_2_use]  #VECTOR OF ERRORS
+    out=np.mean(errors**2.0)                #MSE
+    if(GAMMA_L1!=0.0):
+        out=out+GAMMA_L1*np.sum(np.absolute(p))
+    if(GAMMA_L2!=0.0):
+        out=out+GAMMA_L2*np.sum(p**2.0)
+    return out
 
+
+#------------------------
+#NN_EVAL
+#------------------------
+
+def NN_eval(x,submatrices):
+    Z = x
+    for i in range(0,len(submatrices)):
+        if i % 2 == 0:
+            Z = np.matmul(Z,submatrices[i].T)
+        elif i % 2 == 1:
+            Z = S(Z + submatrices[i].T)
+    return Z
+    
+    
+    
 #------------------------
 #MINIMIZER FUNCTION
 #------------------------
 def minimizer(f,xi):
-	global epoch,epochs, loss_train,loss_val 
-	# x0=initial guess, (required to set NDIM)
-	# algo=GD or MOM
-	# LR=learning rate for gradient decent
+    global epoch,epochs, loss_train,loss_val 
+    # x0=initial guess, (required to set NDIM)
+    # algo=GD or MOM
+    # LR=learning rate for gradient decent
 
-	#PARAM
-	iteration=1			#ITERATION COUNTER
-	NDIM=len(xi)		#DIMENSION OF OPTIIZATION PROBLEM
-	dx_m1=0.0
+    #PARAM
+    iteration=1            #ITERATION COUNTER
+    NDIM=len(xi)        #DIMENSION OF OPTIIZATION PROBLEM
+    dx_m1=0.0
 
-	#OPTIMIZATION LOOP
-	while(iteration<=max_iter):
+    #OPTIMIZATION LOOP
+    while(iteration<=max_iter):
 
-		#-------------------------
-		#DATASET PARITION BASED ON TRAINING PARADIGM
-		#-------------------------
-		if(PARADIGM=='batch'):
-			if(iteration==1): index_2_use=train_idx
-			if(iteration>1):  epoch+=1
+        #-------------------------
+        #DATASET PARITION BASED ON TRAINING PARADIGM
+        #-------------------------
+        if(PARADIGM=='batch'):
+            if(iteration==1): index_2_use=train_idx
+            if(iteration>1):  epoch+=1
 
-		if(PARADIGM=='mini_batch'):
-			#50-50 batch size hard coded
-			if(iteration==1): 
-				#DEFINE BATCHS
-				batch_size=int(train_idx.shape[0]/2)
-				#BATCH-1
-				index1 = np.random.choice(train_idx, batch_size, replace=False)  
-				index_2_use=index1; #epoch+=1
-				#BATCH-2
-				index2 = []
-				for i1 in train_idx:
-					if(i1 not in index1): index2.append(i1)
-				index2=np.array(index2)
-			else: 
-				#SWITCH EVERY OTHER ITERATION
-				if(iteration%2==0):
-					index_2_use=index1
-				else:
-					index_2_use=index2
-					epoch+=1
+        if(PARADIGM=='mini_batch'):
+            #50-50 batch size hard coded
+            if(iteration==1): 
+                #DEFINE BATCHS
+                batch_size=int(train_idx.shape[0]/2)
+                #BATCH-1
+                index1 = np.random.choice(train_idx, batch_size, replace=False)  
+                index_2_use=index1; #epoch+=1
+                #BATCH-2
+                index2 = []
+                for i1 in train_idx:
+                    if(i1 not in index1): index2.append(i1)
+                index2=np.array(index2)
+            else: 
+                #SWITCH EVERY OTHER ITERATION
+                if(iteration%2==0):
+                    index_2_use=index1
+                else:
+                    index_2_use=index2
+                    epoch+=1
 
-		if(PARADIGM=='stocastic'):
-			if(iteration==1): counter=0;
-			if(counter==train_idx.shape[0]): 
-				counter=0;  epoch+=1 #RESET 
-			else: 
-				counter+=1
-			index_2_use=counter
+        if(PARADIGM=='stocastic'):
+            if(iteration==1): counter=0;
+            if(counter==train_idx.shape[0]): 
+                counter=0;  epoch+=1 #RESET 
+            else: 
+                counter+=1
+            index_2_use=counter
 
-		#-------------------------
-		#NUMERICALLY COMPUTE GRADIENT 
-		#-------------------------
-		df_dx=np.zeros(NDIM);	#INITIALIZE GRADIENT VECTOR
-		for i in range(0,NDIM):	#LOOP OVER DIMENSIONS
+        #-------------------------
+        #NUMERICALLY COMPUTE GRADIENT 
+        #-------------------------
+        df_dx=np.zeros(NDIM);    #INITIALIZE GRADIENT VECTOR
+        for i in range(0,NDIM):    #LOOP OVER DIMENSIONS
 
-			dX=np.zeros(NDIM);  #INITIALIZE STEP ARRAY
-			dX[i]=dx; 			#TAKE SET ALONG ith DIMENSION
-			xm1=xi-dX; 			#STEP BACK
-			xp1=xi+dX; 			#STEP FORWARD 
+            dX=np.zeros(NDIM);  #INITIALIZE STEP ARRAY
+            dX[i]=dx;             #TAKE SET ALONG ith DIMENSION
+            xm1=xi-dX;             #STEP BACK
+            xp1=xi+dX;             #STEP FORWARD 
 
-			#CENTRAL FINITE DIFF
-			grad_i=(f(xp1,index_2_use)-f(xm1,index_2_use))/dx/2
+            #CENTRAL FINITE DIFF
+            grad_i=(f(xp1,index_2_use)-f(xm1,index_2_use))/dx/2
 
-			# UPDATE GRADIENT VECTOR 
-			df_dx[i]=grad_i 
-			
-		#TAKE A OPTIMIZER STEP
-		if(algo=="GD"):  xip1=xi-LR*df_dx 
-		if(algo=="MOM"): 
-			step=LR*df_dx+alpha*dx_m1
-			xip1=xi-step
-			dx_m1=step
+            # UPDATE GRADIENT VECTOR 
+            df_dx[i]=grad_i 
+            
+        #TAKE A OPTIMIZER STEP
+        if(algo=="GD"):  xip1=xi-LR*df_dx 
+        if(algo=="MOM"): 
+            step=LR*df_dx+alpha*dx_m1
+            xip1=xi-step
+            dx_m1=step
 
-		#REPORT AND SAVE DATA FOR PLOTTING
-		if(iteration%1==0):
-			predict(xi)	#MAKE PREDICTION FOR CURRENT PARAMETERIZATION
-			# print(iteration,"	",epoch,"	",MSE_T,"	",MSE_V) 
-			print(iteration,"	",xi,"	",MSE_T) 
+        #REPORT AND SAVE DATA FOR PLOTTING
+        if(iteration%1==0):
+            predict(xi)    #MAKE PREDICTION FOR CURRENT PARAMETERIZATION
+            # print(iteration,"    ",epoch,"    ",MSE_T,"    ",MSE_V) 
+            print(iteration,"    ",xi,"    ",MSE_T) 
 
-			#UPDATE
-			epochs.append(epoch); 
-			loss_train.append(MSE_T);  loss_val.append(MSE_V);
+            #UPDATE
+            epochs.append(epoch); 
+            loss_train.append(MSE_T);  loss_val.append(MSE_V);
 
-			#STOPPING CRITERION (df=change in objective function)
-			df=np.absolute(f(xip1,index_2_use)-f(xi,index_2_use))
-			if(df<tol):
-				print("STOPPING CRITERION MET (STOPPING TRAINING)")
-				break
+            #STOPPING CRITERION (df=change in objective function)
+            df=np.absolute(f(xip1,index_2_use)-f(xi,index_2_use))
+            if(df<tol):
+                print("STOPPING CRITERION MET (STOPPING TRAINING)")
+                break
 
-		xi=xip1 #UPDATE FOR NEXT PASS
-		iteration=iteration+1
+        xi=xip1 #UPDATE FOR NEXT PASS
+        iteration=iteration+1
 
-	return xi
+    return xi
 
 
 #------------------------
@@ -289,10 +306,11 @@ def minimizer(f,xi):
 #------------------------
 
 #RANDOM INITIAL GUESS FOR FITTING PARAMETERS
-po=np.random.uniform(0.25,1.,size=NFIT)
+max_rand_wb=1
+po=np.random.uniform(-max_rand_wb,max_rand_wb,size=NFIT)
 
 #TRAIN MODEL USING SCIPY MINIMIZ 
-p_final=minimizer(loss,po)		
+p_final=minimizer(loss,po)        
 print("OPTIMAL PARAM:",p_final)
 predict(p_final)
 
@@ -302,51 +320,51 @@ predict(p_final)
 
 #PLOT TRAINING AND VALIDATION LOSS HISTORY
 def plot_0():
-	fig, ax = plt.subplots()
-	ax.plot(epochs, loss_train, 'o', label='Training loss')
-	ax.plot(epochs, loss_val, 'o', label='Validation loss')
-	plt.xlabel('epochs', fontsize=18)
-	plt.ylabel('loss', fontsize=18)
-	plt.legend()
-	plt.show()
+    fig, ax = plt.subplots()
+    ax.plot(epochs, loss_train, 'o', label='Training loss')
+    ax.plot(epochs, loss_val, 'o', label='Validation loss')
+    plt.xlabel('epochs', fontsize=18)
+    plt.ylabel('loss', fontsize=18)
+    plt.legend()
+    plt.show()
 
 #FUNCTION PLOTS
 def plot_1(xcol=1,xla='x',yla='y'):
-	fig, ax = plt.subplots()
-	ax.plot(X[train_idx][:,xcol]    , Y[train_idx],'o', label='Training') 
-	ax.plot(X[val_idx][:,xcol]      , Y[val_idx],'x', label='Validation') 
-	ax.plot(X[test_idx][:,xcol]     , Y[test_idx],'*', label='Test') 
-	ax.plot(X[train_idx][:,xcol]    , YPRED_T,'.', label='Model') 
-	plt.xlabel(xla, fontsize=18);	plt.ylabel(yla, fontsize=18); 	plt.legend()
-	plt.show()
+    fig, ax = plt.subplots()
+    ax.plot(X[train_idx][:,xcol]    , Y[train_idx],'o', label='Training') 
+    ax.plot(X[val_idx][:,xcol]      , Y[val_idx],'x', label='Validation') 
+    ax.plot(X[test_idx][:,xcol]     , Y[test_idx],'*', label='Test') 
+    ax.plot(X[train_idx][:,xcol]    , YPRED_T,'.', label='Model') 
+    plt.xlabel(xla, fontsize=18);    plt.ylabel(yla, fontsize=18);     plt.legend()
+    plt.show()
 
 #PARITY PLOT
 def plot_2(xla='y_data',yla='y_predict'):
-	fig, ax = plt.subplots()
-	ax.plot(Y[train_idx]  , YPRED_T,'*', label='Training') 
-	ax.plot(Y[val_idx]    , YPRED_V,'*', label='Validation') 
-	ax.plot(Y[test_idx]    , YPRED_TEST,'*', label='Test') 
-	plt.xlabel(xla, fontsize=18);	plt.ylabel(yla, fontsize=18); 	plt.legend()
-	plt.show()
-	
+    fig, ax = plt.subplots()
+    ax.plot(Y[train_idx]  , YPRED_T,'*', label='Training') 
+    ax.plot(Y[val_idx]    , YPRED_V,'*', label='Validation') 
+    ax.plot(Y[test_idx]    , YPRED_TEST,'*', label='Test') 
+    plt.xlabel(xla, fontsize=18);    plt.ylabel(yla, fontsize=18);     plt.legend()
+    plt.show()
+    
 if(IPLOT):
 
-	plot_0()
+    plot_0()
 
-	if(I_UNNORMALIZE):
-		#UNNORMALIZE RELEVANT ARRAYS
-		X=XSTD*X+XMEAN 
-		Y=YSTD*Y+YMEAN 
-		YPRED_T=YSTD*YPRED_T+YMEAN 
-		YPRED_V=YSTD*YPRED_V+YMEAN 
-		YPRED_TEST=YSTD*YPRED_TEST+YMEAN 
+    if(I_UNNORMALIZE):
+        #UNNORMALIZE RELEVANT ARRAYS
+        X=XSTD*X+XMEAN 
+        Y=YSTD*Y+YMEAN 
+        YPRED_T=YSTD*YPRED_T+YMEAN 
+        YPRED_V=YSTD*YPRED_V+YMEAN 
+        YPRED_TEST=YSTD*YPRED_TEST+YMEAN 
 
-	i=0
-	for key in X_KEYS:
-		plot_1(i,xla=key,yla=Y_KEYS[0])
-		i=i+1 
+    i=0
+    for key in X_KEYS:
+        plot_1(i,xla=key,yla=Y_KEYS[0])
+        i=i+1 
 
-	plot_2()
+    plot_2()
 
 
 
