c1f46c7d8843a9ff6e5cc82b9105a1a13dd4096f
Haoming Feng <haomingfeng@haomings-mbp.myfiosgateway.com>
2021-11-14 07:56:15 -0500

    hw6

--- HW6.0/HW6.2.py
+++ HW6.0/HW6.2.py
@@ -22,7 +22,7 @@ print(x_test.shape)
 input = layers.Input(shape = (28,28,1))
 
 
-
+#https://keras.io/examples/vision/autoencoder/
 #Encoder
 x = layers.Conv2D(32,(3,3), activation = 'relu', padding = "same")(input)
 x = layers.MaxPooling2D((2,2), padding = 'same')(x)
@@ -42,4 +42,41 @@ autoencoder.summary()
 #train
 history = autoencoder.fit(x_train,x_train,epochs = 10,validation_data = (x_test,x_test))
 
+
+#ploting loss
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1,len(loss) + 1)
+plt.plot(epochs,loss,'bo',label = 'Training Loss')
+plt.plot(epochs, val_loss,"b",label = "Validation Loss")
+plt.title("Training and validation loss")
+plt.legend()
+plt.savefig('HW6.2_history.png')
+plt.show()
+
+
+#Apply our model to mnist_fashion
+from tensorflow.keras.datasets import fashion_mnist
+(x_train_f, y_train_f),(x_test_f, y_test_f) = fashion_mnist.load_data()
+
+x_train_f = x_train.astype("float32") / 255.
+x_test_f = x_test.astype("float32") / 255.
+
+#Define a counter to count how many anomaly are there in mnist-fashion dataset
+anomaly_counter = 0
+
+#Define our threshold as 4 times our model traning error
+threshold =  4 * autoencoder.evaluate(x_train,x_train)
+
+#https://visualstudiomagazine.com/articles/2019/03/01/neural-anomaly-detection-using-keras.aspx
+#create a for-loop to look at every single image in mnist-fashion and evaluate if it is anomaly
+predicteds = autoencoder.predict(x_train_f)
+for i in range(x_train_f.shape[0]):
+    diff = x_train_f[i] - predicteds[i]
+    curr_se = np.mean(diff * diff)
+    if curr_se > threshold:
+        anomaly_counter += 1
+
+print("total anomaly: ", anomaly_counter)
+print("anomaly percentage: ", anomaly_counter/x_train_f.shape[0] * 100)
 
